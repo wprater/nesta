28a65eed8af1005289985dc21bdf4e488b5399ec
Graham Ashton <graham@effectif.com>
2009-09-01 01:12:22 +0100

    Tweaked the CSS and JavaScript for the Quiz the Market box.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
   var keyword = $.qtm.keywordFromUrl(document.referrer);
   if (keyword) {
     $("h1 + p")
-      .after($("<div/>").addClass("qtm-link")
-        .append($("<p/>").text("So you're researching ")
-          .append($("<span/>").text(unescape(keyword.replace(/\+/g, " "))))
-          .append(", eh?"))
-        .append($("<p/>").text("If this article doesn't solve your problem, why not ")
-          .append($.qtm.link("effectif", "effectif-development", "tell us what you really need"))
-          .append("? Maybe we'll write that article for you&hellip;")));
+      .after($("<div/>")
+        .addClass("qtm-link")
+        .append($("<p/>").text("We see you're researching ")
+        .append($("<span/>").text(unescape(keyword.replace(/\+/g, " "))))
+        .append("."))
+        .append($("<p/>")
+          .text("If this page doesn't solve your problem, why not ")
+          .append($.qtm.link(
+              "effectif", "effectif-development", "tell us what you really need"))
+          .append("? We'd&nbsp;love to write that article for you&hellip;"))
+        .hide()
+        .fadeIn(1000));
   }
 });
