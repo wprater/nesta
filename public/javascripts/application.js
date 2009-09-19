7466221dec066ead1c406d6561f6ed1acd221c8a
Graham Ashton <graham@effectif.com>
2009-09-19 21:01:23 +0100

    Removed last vestiges of QtM JavaScript, shrank 'Development' in header.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,6 @@
 $(document).ready(function() {
-  var keyword = $.qtm.keywordFromUrl(document.referrer);
-  if (keyword) {
-    $("h1 + p")
-      .after($("<div/>")
-        .addClass("qtm-link")
-        .append($("<p/>").text("We see you're researching ")
-        .append($("<span/>").text(unescape(keyword.replace(/\+/g, " "))))
-        .append("."))
-        .append($("<p/>")
-          .text("If this page doesn't solve your problem, why not ")
-          .append($.qtm.link(
-            "effectif", "effectif-development", "tell us what you really need"))
-          .append("? We'd&nbsp;love to write that article for you&hellip;"))
-        .hide()
-        .fadeIn(1000));
-  }
+  var heading = $("#logo a");
+  var words = heading.text().split(" ");
+  var span = "<span>" + words.slice(1, words.length).join(" ") + "</span>";
+  heading.html(words[0] + " " + span);
 });
